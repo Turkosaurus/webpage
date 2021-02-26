c26af03a168ee27d2267112f4cc97a1b89db8bfa
Travis Turk <travisturk@gmail.com>
2021-02-25 18:31:22 -0600

    all pages outlined

--- application.py
+++ application.py
@@ -1,28 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
-# # Ensure templates are auto-reloaded
-# app.config["TEMPLATES_AUTO_RELOAD"] = True
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/bio")
-def index():
-    return render_template("bio.html")
-
 @app.route("/projects")
-def index():
+def projects():
     return render_template("projects.html")
 
-@app.route("/projects")
-def index():
-    return render_template("projects.html")
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
+@app.route("/pdf")
+def pdf():
+    return send_file('static/resume_TravisTurk_web.pdf', attachment_filename='resume.TravisTurk.pdf') 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
-# if __name__ == '__main__':
-#     app.run()
+# # if __name__ == '__main__':
+# #     app.run()
