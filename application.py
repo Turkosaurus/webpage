2015db0a4d5cc9ad21ae0d22210b4e912d94b38b
Travis Turk <travisturk@gmail.com>
2021-02-25 19:42:20 -0600

    buttons added

--- application.py
+++ application.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
+# # Ensure templates are auto-reloaded
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
@@ -20,7 +19,7 @@ def resume():
 
 @app.route("/pdf")
 def pdf():
-    return send_file('static/resume_TravisTurk_web.pdf', attachment_filename='resume.TravisTurk.pdf') 
+    return send_file('static/resume_TravisTurk_web.pdf', attachment_filename='resume.TravisTurk.pdf')
 
 @app.route("/contact")
 def contact():
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Personal Webpage with Flask
+
+## TODOs
+- complete copy
+- adjust style
+- deploy to Heroku
+- migrate personal URL
