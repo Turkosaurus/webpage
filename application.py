0650dd4ce7dffdcba827f709b2c5deb0024b3724
Travis Turk <travisturk@gmail.com>
2021-04-11 21:43:44 -0500

    homepage and project naviation added

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, url_for
+from flask import Flask, render_template, send_file, url_for, redirect
 
 app = Flask(__name__)
 
@@ -6,13 +6,19 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
 @app.route("/")
 def home():
+    # return redirect("/bio")
     return render_template("index.html")
 
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+
+@app.route("/bio")
+def bio():
+    return render_template("bio.html")
 
 @app.route("/resume")
 def resume():
@@ -22,9 +28,9 @@ def resume():
 def pdf():
     return send_file('static/resume_TravisTurk_web.pdf', attachment_filename='resume.TravisTurk.pdf')
 
-@app.route("/wx")
-def wx():
-    return render_template("wx.html")
+# @app.route("/wx")
+# def wx():
+#     return render_template("wx.html")
 
 # # if __name__ == '__main__':
 # #     app.run()
