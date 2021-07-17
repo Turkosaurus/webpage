6a5ad22239c6c4c47318906cadb4e272709ef203
Travis Turk <travisturk@gmail.com>
2021-07-17 00:41:45 -0500

    redesign first pass, added pictures/gradients and logos

--- application.py
+++ application.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, send_file, url_for, redirect
+from flask import Flask, render_template, send_file, url_for, redirect, flash
 
+import os
+import threading
+import discord
+from dotenv import load_dotenv
+
+
+
+# Flask App
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-
 @app.route("/")
 def home():
     # return redirect("/bio")
     return render_template("index.html")
 
-@app.route("/projects")
-def projects():
+@app.route("/portfolio")
+def portfolio():
     return render_template("projects.html")
 
 @app.route("/bio")
@@ -28,9 +35,35 @@ def resume():
 def pdf():
     return send_file('static/resume_TravisTurk_web.pdf', attachment_filename='resume.TravisTurk.pdf')
 
+
 # @app.route("/wx")
 # def wx():
 #     return render_template("wx.html")
 
 # # if __name__ == '__main__':
 # #     app.run()
+
+# tWebpage = threading.Thread(target=webpage)
+# tWebpage.start()
+
+# tWebpage = threading.Thread(target=webpage)
+# tWebpage.start()
+
+# load_dotenv()
+
+# def cluck():
+#     print("Waking Cluck...")
+
+#     TOKEN = os.getenv('DISCORD_TOKEN')
+
+#     client = discord.Client()
+
+#     @client.event
+#     async def on_ready():
+#         print(f'{client.user} has connected to Discord!')
+
+#     client.run(TOKEN)
+#     flash("Cluck is in the coop.")
+
+# tCluck = threading.Thread(target=cluck)
+# tCluck.start()
