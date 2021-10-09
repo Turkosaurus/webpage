a24159059fb5acaaed9c44f48ed75c0dcdf9c0ff
Travis Turk <travisturk@gmail.com>
2021-10-08 23:44:13 -0500

    further configuration adjustments

--- application.py
+++ application.py
@@ -31,13 +31,36 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
+# https://elements.heroku.com/buildpacks/pyronlaboratory/heroku-integrated-firefox-geckodriver
+def  load_driver():
+	options = webdriver.FirefoxOptions()
+	
+	# enable trace level for debugging 
+	options.log.level = "trace"
+
+	options.add_argument("-remote-debugging-port=9224")
+	options.add_argument("-headless")
+	options.add_argument("-disable-gpu")
+	options.add_argument("-no-sandbox")
+
+	binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
+
+	firefox_driver = webdriver.Firefox(
+		firefox_binary=binary,
+		executable_path=os.environ.get('GECKODRIVER_PATH'),
+		options=options)
+
+	return firefox_driver
+
+
 def punch():
 
     # Selenium options
-    opts = Options()
-    opts.set_headless()
-    assert opts.headless  # Operating in headless mode
-    browser = Firefox(options=opts, executable_path='/app/vendor/geckodriver/geckodriver')
+    # opts = Options()
+    # opts.set_headless()
+    # assert opts.headless  # Operating in headless mode
+    # browser = Firefox(options=opts)
+    browser = load_driver()
     browser.get('https://workforcenow.adp.com/theme/index.html#/Myself_ttd_MyselfTabTimecardsAttendanceSchCategoryMyTimeEntry/MyselfTabTimecardsAttendanceSchCategoryMyTimeEntry')
 
     # Login to ADP
