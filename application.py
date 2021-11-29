af815c28686806a00003897c49d5e0836c37b221
Travis Turk <travisturk@gmail.com>
2021-11-28 20:13:09 -0600

    twilio validate decorator

--- application.py
+++ application.py
@@ -45,6 +45,29 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config['UPLOAD_FOLDER'] = os.getenv('PWD') + "/static/uploads"
 
+# https://www.twilio.com/docs/usage/tutorials/how-to-secure-your-flask-app-by-validating-incoming-twilio-requests#
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
+        if request_valid:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
 def  load_driver():
     # https://elements.heroku.com/buildpacks/pyronlaboratory/heroku-integrated-firefox-geckodriver
 
@@ -267,6 +290,7 @@ def ping():
 
 
 @app.route("/message", methods=['POST'])
+@validate_twilio_request
 def message():
 
     """Send a dynamic reply to an incoming text message"""
