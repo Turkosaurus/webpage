907a746aac09aa03e3dd4c56d67bb6cf304e1afd
Travis Turk <travisturk@gmail.com>
2022-02-21 00:23:52 -0600

    more work on scripts.js to outline all steps

--- static/scripts.js
+++ static/scripts.js
@@ -1,20 +1,50 @@
-function difference (weightStart, weightEnd, kegType) {
+const beerDensity = 8.345 // Density of beer in pounds/gallon
 
-
-
-    // Match kegType to kegWeights data to find base weight
-    // pounds
-    const kegWeights = {
-        "1/2 BBL":69
+const kegData = {
+    "1/2 BBL": {
+        "capacityGal":15.5,
+        "weightEmpty":31
+    },
+    "1/3 BBL": {
+        "capacityGal":13.2,
+        "weightEmpty":27
+    },
+    "1/4 BBL": {
+        "capacityGal":7.9,
+        "weightEmpty":18
+    },
+    "1/6 BBL": {
+        "capacityGal":5.2,
+        "weightEmpty":14.5
     }
+}
 
-    let weightDelta = weightStart - weightEnd
 
+// PSEUDOCODE
+
+// Match kegType to kegWeights data to find base weight
+
+// get keg size
+    // populate minimum and maximum starting weight and ending weight
+
+function difference (kegType, weightGross) {
+
+    // match kegType to kegData
+    let capacityGal = kegData[kegType]['capacityGal'];
+    let weightEmpty = kegData[kegType]['weightEmpty'];
+
+    // Calculate usage
+    let weightMax = capacityGal * beerDensity; // Weight of beer within a full keg
+    let weightLiquid = weightGross - weightEmpty; // Subtract shell weight
+
+    // Beer remaining
+    let percent = weightLiquid / weightMax;
+    let gallons = weightLiquid / 8.345;
+    
     let results = {
-        'weightUsed':weightDelta,
-        'percentUsed':'foo',
-        'ounces used':'3.14'
-    }
+        'percent':percent,
+        'galons':gallons
+    };
 
-    return results
+    return results;
 }
