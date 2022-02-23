ded87972b905b5a410e8bb58dbcdd97626d1c2cb
Travis Turk <travisturk@gmail.com>
2022-02-22 21:03:26 -0600

    functional keg weight utility!

--- static/scripts.js
+++ static/scripts.js
@@ -19,13 +19,76 @@ const kegData = {
     }
 }
 
+document.addEventListener('DOMContentLoaded', loadListeners, false);
+// document.addEventListener('DOMContentLoaded', loadData, false);
+
+function loadListeners() {
+    console.log("Hello!");
+
+    const weightBegin = document.getElementById("weightBegin");
+    const weightEnd = document.getElementById('weightEnd');
+    const kegSize = document.getElementById('kegSize');
+    
+    kegSize.addEventListener('change', (event) => {
+        let size = event.target.value;
+        let min = kegData[size].weightEmpty;
+        let shell = kegData[size].weightEmpty
+        let max = (kegData[size].capacityGal * beerDensity) + shell;
+        max = Number.parseFloat(max).toFixed(2);
+        console.log(max);
+        
+        weightBegin.min = min;
+        weightBegin.max = max;
+        weightBegin.value = max;
+
+        weightEnd.min = min;
+        weightEnd.max = max;
+        
+        // weightEnd.value = "";
+        console.log(event.target.value)
+    });
+
+    const percentUsed = document.getElementById('percentUsed');
+    weightEnd.addEventListener('change', (event) => {
+        // if zero, insert placeholder
+        let size = kegSize.value
+        let shell = kegData[size].weightEmpty
+        result = (weightEnd.value - shell) /  (weightBegin.max - shell)
+        result = Number.parseFloat(result * 100).toFixed(2);
+        // console.log(result)
+        // percentUsed.value = "foo"
+        percentUsed.value = `${result}%`
+    });
+}
+
+function updatePercent() {
+
+}
+
+
+function updateKegMinMax() {
+    console.log("HOLY HELL WE MADE IT");
+    alert("we made it");
+}
+
+
+
+// event listener for keg size, set min
+    // starting weight is maximum possible (or TODO the saved, cached last value for that beer)
+    // populate minimum and maximum starting weight and ending weights
+
+function nextKeg() {
+    // add a row to the results
+    // clear the form
+
+}
+
 
 // PSEUDOCODE
 
 // Match kegType to kegWeights data to find base weight
 
 // get keg size
-    // populate minimum and maximum starting weight and ending weight
 
 function difference (kegType, weightGross) {
 
