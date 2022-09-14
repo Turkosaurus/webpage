86c5255fe6254ec08d8db0647230a1966604810d
Travis Turk <55367187+Turkosaurus@users.noreply.github.com>
2022-09-13 22:16:38 -0500

    improvements to reminders

--- cron_tasks.py
+++ cron_tasks.py
@@ -1,7 +1,7 @@
 """ Completes cron tasks from Heroku Scheduler. """
 import os
 # import time
-# import datetime
+import datetime
 import psycopg2
 import psycopg2.extras
 # import requests
@@ -12,20 +12,63 @@ load_dotenv()
 # PostgreSQL database connection
 db = os.getenv('DATABASE_URL')
 
-print("Running cron_tasks.py...")
+now = datetime.datetime.now()
+print(f"Running cron_tasks.py at {now}...")
+print(now.strftime("%H:%M:%S:%w"))
+current_time = {
+    "day":now.strftime("%w"),
+    "hour":now.strftime("%H"),
+    "minute":now.strftime("%H"),
+}
+print(current_time)
 
-def main():
+def reminders_fetch():
     """ Checks for scheduled tasks. """
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
 
             # Download file by id, structure metadata
-            cur.execute("SELECT * FROM reminders_recurring")
-            data = cur.fetchone()
+            cur.execute("SELECT * FROM reminder_schedule")
+            data = cur.fetchall()
             cur.close()
-            print(f"Recurring reminders fetched!\n{data}")
+            print(f"Recurring reminders fetched!")
+
+            reminders = []
+            for row in data:
+                reminders.append(dict(row._asdict()))
+
+            # for key, val in data:
+            #     reminder = {}
+            #     reminders.append(reminder)
+            #     reminder[key] = val
+            # for d in data:
+            #     print(d.reminder_id, d.day, d.hour, d.minute, d.reminded)
+            #     reminders.reminder_id = d.reminder_id
+            #     reminders.day = d.day
+            #     reminders.hour = d.hour
+            #     reminders.minute
+                # print(f"{d['reminder_id']} {d['day']} {d['hour']} {d['minute']} {d['reminded']}")
+            
+            print("reminders:")
+            for reminder in reminders:
+                print(reminder.values())
 
     conn.close()
+
+    return reminders
+
+def reminders_queue():
+    print("todo")
+
+def main():
+    """ Runs scheduled functions. """
+    try:
+        reminders = reminders_fetch()
+        # queue = reminders_queue(reminders)
+        # reminders_send(queue)
+
+    except Exception as e:
+        print(f"Error!: {e}")
 
     # content = "foo"
     # r = requests.get('https://www.turkosaur.us/ping/%s' % content)
--- database.py
+++ database.py
@@ -1,9 +1,10 @@
-""" Handles application database logic. """
 import os
 import psycopg2
 import psycopg2.extras
 from dotenv import load_dotenv
 load_dotenv()
+
+""" Handles application database logic. """
 
 # PostgreSQL database connection
 db = os.getenv('DATABASE_URL')
@@ -12,24 +13,98 @@ def create_tables():
     """ Setup database tables. """
     with psycopg2.connect(db) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
-            cur.execute("CREATE TABLE IF NOT EXISTS reminders_recurring ( \
+            cur.execute("CREATE TABLE IF NOT EXISTS reminders ( \
                 reminder_id SERIAL, \
+                text VARCHAR(1024), \
                 num_id INTEGER, \
-                name VARCHAR(1024), \
-                interval_hours INTEGER \
+                active BOOL \
             )")
 
-            cur.execute("CREATE TABLE IF NOT EXISTS reminders_queue ( \
+            cur.execute("CREATE TABLE IF NOT EXISTS reminder_schedule ( \
                 reminder_id INTEGER, \
-                due TIMESTAMP, \
-                pokes INTEGER, \
-                completed BOOL, \
+                day INTEGER, \
+                hour INTEGER, \
+                minute INTEGER, \
+                reminded INTEGER, \
+                active BOOL, \
                 completed_on TIMESTAMP \
             )")
 
+def load_tables():
+    """ Setup database tables. """
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+            cur.execute("INSERT INTO reminders (text, num_id, active) \
+                VALUES ('apix 5mg', 5, true) \
+            ")
+
+            weekly = [ # [day, hour, minute]
+                [0, 10, 00],
+                [0, 20, 00],
+                [1, 10, 00],
+                [1, 20, 00],
+                [2, 10, 00],
+                [2, 20, 00],
+                [3, 10, 00],
+                [3, 20, 00],
+                [4, 10, 00],
+                [4, 20, 00],
+                [5, 10, 00],
+                [5, 20, 00],
+                [6, 10, 00],
+                [6, 20, 00]
+            ]
+
+            for reminder in weekly:
+                # Twice daily for a week
+                cur.execute(f"INSERT INTO reminder_schedule \
+                    (reminder_id, day, hour, minute) \
+                    VALUES (1, {reminder[0]}, {reminder[1]}, {reminder[2]}) \
+                ")
+
+def backup_tables():
+    """ Setup database tables. """
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+            tables = [
+                "nail_boxes",
+                "nail_boxprod",
+                "nail_boxused",
+                "nail_colors",
+                "nail_cycles",
+                "nail_items",
+                "nail_loterias",
+                "nail_parts",
+                "nail_projections",
+                "nail_queueitems",
+                "nail_queueparts",
+                "nail_shirts",
+                "nail_sizes",
+                "nail_types",
+                "nail_users"
+            ]
+            for table in tables:
+                # backup table
+                cur.execute(f"DROP TABLE IF EXISTS {table}_backup")
+                cur.execute(f"CREATE TABLE IF NOT EXISTS {table}_backup as \
+                    (SELECT * from {table}) \
+                ")
+    conn.close()
+
+def drop_tables():
+    """ Setup database tables. """
+    with psycopg2.connect(db) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor) as cur:
+            cur.execute("DROP TABLE IF EXISTS reminders")
+            cur.execute("DROP TABLE IF EXISTS reminder_schedule")
+    conn.close()
+
 def main():
     """ Main function. """
+    drop_tables()
     create_tables()
+    load_tables()
+    # backup_tables()
 
 if __name__ == '__main__':
     main()
